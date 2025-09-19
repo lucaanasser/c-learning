8056791f0634852b243b67c79bf0bc8c47bd9ed0
lucaanasser <lucamarinhonasser@usp.br>
2025-09-18 22:42:03 -0300

    Implementacao lista ligada - EP1

--- EP1/list.c
+++ EP1/list.c
@@ -1,16 +1,43 @@
-typedef struct List {
-    /* PREENCHA */
-} List;
+typedef struct Node {
+  int data;
+  struct Node *next;
+}List;
 
-List* create_list(){
+List* new_node(int data){
+  List *new_node = malloc(sizeof(List));
+  new_node->data = data;
+  new_node->next = NULL;
+  return new_node;
+}
+
+List* insert(List *ptr, int data){
+  List *head = ptr;
+  List *insert_node = new_node(data);
+  insert_node->next = head;
+  head = insert_node;
+  return head;
+}
+
+List* append(List *ptr, int data){
+  List *head = ptr;
+  List *tail = head;
+  List *append_node = new_node(data);
+  
+  if (head == NULL){
+    head = append_node;
+    tail = append_node;
+  }else{
+    tail->prox = append_node;
+    tail = append_node;
+  }
+}
+
+int exclude_node(List *ptr, int data){
   
 }
 
-void creat_node(){
-  
+bool empty_list(list L){
+  return(L == NULL) ? TRUE : FALSE;
 }
 
-int exclude_node(){
-  
-}
 
